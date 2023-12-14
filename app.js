a62109170230309b1fc07e16a3cb695caefbf1da
= <arparvez50@gmail.com>
2023-12-14 15:15:53 +0600

    First Commit

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
-let card = document.querySelector(".card");
-let modalOpenBtn = document.querySelector(".card button");
-let modal = document.querySelector(".modal");
-let modalCrossBtn = document.querySelector(".modal button");
+let students = document.querySelector(".students");
+let showStudents = document.querySelector(".show__students");
+let displayText = document.querySelector(".display");
+let items = document.querySelectorAll(".list-group-item");
+let runContent = document.querySelector(".run__content");
+let resetContent = document.querySelector(".reset__content");
 
-modalOpenBtn.addEventListener("click", function (event) {
-    modal.classList.remove("d-none");
+// for getting random number by arr size
+function getRandomPerson(arr) {
+    let length = arr.length;
+    let randomNumber = Math.floor(Math.random() * length);
+    return randomNumber;
+}
+
+
+// Show students output
+showStudents.addEventListener('click',function(event){
+    let studentsArray = students.value.split(",").sort();
+
+    displayText.innerText = studentsArray.join(' ')
+    
+})
+
+// Run the lottery
+runContent.addEventListener("click", function (event) {
+    let studentsArray = students.value.split(",").sort();
+    loopPerson(studentsArray);
 });
 
-modalCrossBtn.addEventListener("click", function (event) {
-    modal.classList.add("d-none");
+
+// Reset the lottery
+resetContent.addEventListener("click", function (event) {
+    location.reload();
 });
+
+function loopPerson(arr) {
+    let personArray = [...items];
+    for (let i = 0; i < personArray.length; i++) {
+        let children = personArray[i].children;
+        let childrenArray = [...children];
+        setTimeout(function () {
+            let winner = arr[getRandomPerson(arr)];
+            childrenArray[0].innerText = winner;
+        }, 2000 * i);
+    }
+}
+
